4ddf609b10dce24c37b2165e75d16041af1094b6
efajunk <efa.junk@gmail.com>
2021-07-11 20:32:21 +0300

    m5_t2 solved

--- Python 1/Day 5/Homework/m5_t2.py
+++ Python 1/Day 5/Homework/m5_t2.py
@@ -10,3 +10,16 @@ a3 b3 c3 d3
 a2 b2 c2 d2
 a1 b1 c1 d1
 '''
+import string
+
+size = int(input('Введите размер шахматной доски от 0 до 20: '))
+
+alpha = string.ascii_lowercase
+
+# digits = [i for i in range(size, 0, -1)]
+
+for i in range(size, 0, -1):
+    for j in range(size):
+        print(alpha[j], i, sep='', end=' ')
+#         print(alpha[j], digits[i], sep='', end=' ')
+    print('')
