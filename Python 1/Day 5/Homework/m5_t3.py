184d9b67d9ea922a5a99044c0a6b5cb22bc1536c
efajunk <53151777+efajunk@users.noreply.github.com>
2021-07-12 19:45:43 +0300

    Update m5_t3.py
    bottles_find check added

--- Python 1/Day 5/Homework/m5_t3.py
+++ Python 1/Day 5/Homework/m5_t3.py
@@ -14,9 +14,9 @@ Out: 22 бутылки
 
 endings = ['ок', 'ка', 'ки']
 bottles_count = int(input('Введите количество бутылок: '))
-bottle_word = 'бутыл'
+bottles_find = bottles_count % 100
 
-if 21 > bottles_count > 4 or 4 < (bottles_count % 10) < 10:
+if 21 > bottles_find > 4 or 4 < (bottles_find % 10) < 10:
     print(f'{bottles_count} {bottle_word}{endings[0]}')
 elif bottles_count % 10 == 1:
     print(f'{bottles_count} {bottle_word}{endings[1]}')
